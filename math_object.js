fc3fcd4da79a461f39246661ad037dee1ac14a3e
Sebastian Górecki <gorecki.sebastian@gmail.com>
2015-01-25 13:13:35 +0100

    Preview of the functions from Math object.

---
+++ math_object.js
@@ -0,0 +1,29 @@
+var power = Math.pow(2, 8); // 8
+
+var sqrt = Math.sqrt(9); // 3
+
+var round1 = Math.round(9.13); // 9
+var round2 = Math.round(9.53); // 10
+
+var ceil = Math.ceil(9.03); // 10
+
+var floor = Math.floor(9.99); // 9
+
+var max = Math.max(20, 45, 1, 45, 75); // 75
+
+var min = Math.min(20, 45, 1, 45, 75); // 1
+
+var abs = Math.abs(-3.14); // 3.14
+
+
+
+var random1 = Math.random(); // random <0, 1);
+var random2 = Math.floor(Math.random() * 10); // random <0, 10)
+
+
+// constants
+var pi = Math.PI;
+var sqrt2 = Math.SQRT2;
+
+
+
