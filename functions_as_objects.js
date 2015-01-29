4bae957a854c4457859364ddba28a37ad0aa3ae0
Sebastian Górecki <gorecki.sebastian@gmail.com>
2015-01-29 22:56:38 +0100

    Added very flexible Javascript mechanism to getting arguments in functions and use of Apply and/or Call methods.

--- functions_as_objects.js
+++ functions_as_objects.js
@@ -1,17 +1,26 @@
-var calculate = function(x, y, fn) {
-    return fn(x, y);
+var calculate = function() {
+    var fn = Array.prototype.pop.apply(arguments); // returning last argument and remove it from array
+    return fn.apply(null, arguments);
 };
 
-var sum = function(x, y) {
-    return x + y;
+var sum = function() {
+    var total = 0;
+    for (var i = 0, len = arguments.length; i < len; i = i + 1) {
+        total = total + arguments[i];
+    }
+    return total;
 };
 
-var diff = function(x, y) {
-    return x - y;
+var diff = function() {
+    var total = Array.prototype.shift.apply(arguments); // first element of the array and remove it from array
+    for (var i = 0, len = arguments.length; i < len; i = i + 1) {
+        total = total - arguments[i];
+    }
+    return total;
 };
 
-var sumResult = calculate(1, 2, sum),
-    diffResult = calculate(1, 2, diff);
+var sumResult = calculate(1, 2, 3, 4, 5, sum),
+    diffResult = calculate(1, 2, 3, 4, 5, diff);
 
 alert(sumResult);
 alert(diffResult);
